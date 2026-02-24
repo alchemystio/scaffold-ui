940143218f065a9cbca1ab9d4ff6631e0a9e7672
Sheikh-Abdul-Aziz <sheikhabdulaziz.ml@gmail.com>
2026-02-24 21:17:33 +0530

    .

--- src/index.ts
+++ src/index.ts
@@ -26,5 +26,6 @@ import './tailwind/theme.css';
 export * from './components/index';
 export * from './hooks/index';
 export * from './layout/index';
+export * from './primitives/index';
 export * from './semantic/index';
-export * from './typography/index';
+export * from './tokens/index';
